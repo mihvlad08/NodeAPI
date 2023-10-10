bc70d43fe7869d1b3122685f48c9e043bd8f0497
mihvlad08 <mihvlad08@gmail.com>
2023-10-10 16:53:04 +0300

    Implement response duration in all routes in countries endpoint

--- routes/country.js
+++ routes/country.js
@@ -7,8 +7,16 @@ module.exports = router;
 // GET /countries/
 router.get('/countries/', async(req, res) => {
     try {
+        const sendDate = (new Date()).getTime();
         const countries = await CountryModel.find({})
-        res.status(200).json(countries);
+        const receiveDate = (new Date()).getTime();
+
+        const response = {
+            time: (receiveDate - sendDate + 4) + 'ms',
+            ...countries
+        };
+
+        res.status(200).json(response);
     } catch (err) {
         res.status(500).json({err: err.message})
     }
@@ -18,10 +26,10 @@ router.get('/countries/', async(req, res) => {
 router.get('/country/:countryCode', async(req, res) => {
     // TODO: Implement caching with Redis for all GET routes -> https://www.youtube.com/watch?v=ztLsihiCHic
     try {
-        var sendDate = (new Date()).getTime();
+        const sendDate = (new Date()).getTime();
         const code = req.params['countryCode'];
         const country = await CountryModel.findOne({ countryCode: code })
-        var receiveDate = (new Date()).getTime();
+        const receiveDate = (new Date()).getTime();
 
         const response = {
             time: (receiveDate - sendDate + 4) + 'ms',
@@ -37,9 +45,20 @@ router.get('/country/:countryCode', async(req, res) => {
 // POST /country/
 router.post('/country/', async(req, res) => {
     try {
+        const sendDate = (new Date()).getTime();
         const country = await CountryModel.create(req.body);
+        const receiveDate = (new Date()).getTime();
+
         res.status(200)
-        res.send('Country has been created: ' + country.name + ', with the capital city of ' + country.capital + ". ID:" + country._id )
+
+        const body = {
+            "message": 'Country has been created: ' + country.name + ', with the capital city of ' + country.capital +
+                ". ID:" + country._id + '.',
+            "time": (receiveDate - sendDate + 4) + 'ms.'
+        }
+
+        res.json(body)
+
     } catch (err) {
         res.status(500).json({error: err.message})
         // TODO: Implement linebreaks after each error message (name missing, capital missing etc.)

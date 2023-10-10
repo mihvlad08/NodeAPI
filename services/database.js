35013d928d7027acf3d25e81caec6a629877c330
mihvlad08 <mihvlad08@gmail.com>
2023-10-10 15:21:33 +0300

    Preparing deployment - env variables

--- services/database.js
+++ services/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose
-.connect('mongodb+srv://admin:admin@nodeapi.kczy3fx.mongodb.net/NodeAPI?retryWrites=true&w=majority')
-.then(() => {
-    console.log('Successfully connected to the database');
-}).catch(() => {
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nodeapi.kczy3fx.mongodb.net/NodeAPI?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('Successfully connected to the database');
+    }).catch(() => {
     console.log('Failed to connect to the database');
 })
 

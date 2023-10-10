1d638f582473d68125849a9843b0fe5ae5358e67
mihvlad08 <mihvlad08@gmail.com>
2023-10-10 15:28:59 +0300

    Add root route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.listen(3000, () => {
     console.log('Listening on port 3000');
 })
 
+app.get('/', (res, req) => {
+    res.status(200).json('API Connection successfully established.')
+})
+
 const databaseConnector = require('./services/database');
 
 app.use(express.json())
